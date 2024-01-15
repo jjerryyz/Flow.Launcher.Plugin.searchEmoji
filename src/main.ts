1fba336bb0e6833d2172045d1374caf8a88cce02
jerryzheng <zheng.jianli@outlook.com>
2024-01-16 07:26:20 +0800

    add fuzzy search

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import emojis from './lib/emoji.json';
 import { Flow } from "./lib/flow";
 import logger from "./lib/logger";
 import { execSync } from "child_process";
+import Fuse from 'fuse.js';
 
 // The events are the custom events that you define in the flow.on() method.
 const events = ["search"] as const;
@@ -9,36 +10,23 @@ type Events = (typeof events)[number];
 
 const flow = new Flow<Events>();
 
+const fuze = new Fuse(emojis, { keys: ['description', { name: 'tags', weight: 2 }, { name: 'aliases', weight: 2 }] })
 
 flow.on("query", (params = []) => {
 
 	const [query] = params as string[];
-
-	let results: typeof emojis = []
 
 
 	if (!query) {
 		return flow.showInputHint();
 	}
 
-	try {
-		results = emojis.filter(emoji => {
-			if (emoji.description.includes(query)) {
-				return true;
-			}
-			if (emoji.aliases.some(alias => alias.includes(query))) {
-				return true
-			}
-		})
-	} catch (error) {
-	}
-
-	const result = results.map(r => {
+	const result = fuze.search(query).map(({ item: r }) => {
 		// transform emoji to something like 
 		const flavor = 'img-google-136';
 		const { emoji, variant: _variant } = r;
-		const variant = 
-		_variant === 1 ? `-fe0e` : _variant === 2 ? '-fe0f' : '';
+		const variant =
+			_variant === 1 ? `-fe0e` : _variant === 2 ? '-fe0f' : '';
 		const code = emoji.codePointAt(0)?.toString(16);
 		return {
 			Title: r.description,
